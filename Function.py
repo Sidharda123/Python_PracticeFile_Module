3772210a502dbc7160094d154f6421cad0455bc1
Sidharda123 <21pa1a0517@vishnu.edu.in>
2025-10-23 11:35:43 +0530

    add function problem  to Repo

---
+++ Function.py
@@ -0,0 +1,16 @@
+def add(a, b):
+    return a + b
+
+def add_multiply(a, b):
+    c = add(a, b)
+    d = c * 2
+    print(d)
+    return d
+
+def add_div(a, b):
+    c = add(a, b)
+    e = c / 2
+    print(e)
+    return e
+
+add_div(4,6)
